02ef870c605e4b8ea621080faaf4659212a58fc0
이준서 <henryjoon2005@gmail.com>
2025-08-12 13:59:03 +0900

    Complete Level3

--- Source/app.py
+++ Source/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import os
+import socket
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -7,12 +8,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 app = Flask(__name__, template_folder=TEMPLATES_DIR)
 
 @app.route("/") 
-def index():
-    return "Hello World"
-
-@app.route("/menu")
-def menu():
-    return render_template("menu.html")
+def home():
+    if app.debug:
+        hostname = '컴퓨터(인스턴스) : ' + socket.gethostname()
+    else:
+        hostname = ' '
+    return render_template("index.html", computername=hostname)
 
 if __name__ == "__main__": 
-    app.run(host="0.0.0.0", port=80) 
+    app.run(host="0.0.0.0", port=80, debug=True) 
