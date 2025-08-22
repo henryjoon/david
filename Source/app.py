4b39108b928b36cda156d85f3ec539238fb4fa2c
henryjoon <35223443+henryjoon@users.noreply.github.com>
2025-08-22 10:51:55 +0900

    Update app.py

--- Source/app.py
+++ Source/app.py
@@ -19,5 +19,9 @@ def home():
 def menu():
     return render_template("menu.html")
 
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
+
 if __name__ == "__main__": 
     app.run(host="0.0.0.0", port=80, debug=True) 
